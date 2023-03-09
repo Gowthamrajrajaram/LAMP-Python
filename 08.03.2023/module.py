a4a1db2af3fb6d09531815eb776c6247edb83d7c
Gowthamrajrajaram <gowthamraj250502@gmail.com>
2023-03-09 09:21:18 +0530

    modules

---
+++ 08.03.2023/module.py
@@ -0,0 +1,30 @@
+
+class vehicle:
+    def bike(self,enginetype,coolingsys):
+        self.enginetype=enginetype
+        self.coolingsys=coolingsys
+        print("You are using a honda bike. It is type of",enginetype,"stroke engine and The engine is "+coolingsys+" cooling system")
+    def car(self,engtype,cooling):
+         self.engtype=engtype
+         self.cooling=cooling
+         print("You are using a KIA car. It is type of",engtype,"stroke engine and The engine is "+cooling+" cooling system")
+
+def fun(num):
+    if num%2==0:
+        print("odd number: ",num)
+    else:
+        print("Even number: ",num)
+
+
+def count_case(words):
+    dic={ 'count':0,
+       'num':0}
+    for val in words:
+        if val.isupper():
+           dic['count']+=1
+        elif val.islower():
+            dic['num']+=1
+        else:
+            pass
+    print("No. of Upper case characters : ",dic['count'])
+    print("No. of Lower case Characters : ",dic['num'])
---
+++ 08.03.2023/modulesmain.py
@@ -0,0 +1,10 @@
+import module as m
+
+m.transport=m.vehicle()
+m.transport.bike(2,"air")
+m.transport.car(4,"oil")
+
+print("..............................")
+m.fun(5)
+print("..............................")
+m.count_case("The quick Brow Fox")
